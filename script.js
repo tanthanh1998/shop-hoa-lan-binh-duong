c66c15997a879640360497f6810f2b7309ecd551
thanhnguyen <thanh.nguyen@go2joy.vn>
2025-06-20 23:52:40 +0700

    Update nmha

--- script.js
+++
@@ -1,3 +0,0 @@
-// Hiện tại không cần JS đặc biệt, nhưng bạn có thể thêm tương tác sau này.
-// Ví dụ: scroll về mục sản phẩm, hiệu ứng hoặc form liên hệ.
-console.log("Website hoa lan đã được tải!");
