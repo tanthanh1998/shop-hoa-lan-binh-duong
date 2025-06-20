74a0482ebdbb506a65a8c87b2d5ff8712d74c3a9
thanhnguyen <thanh.nguyen@go2joy.vn>
2025-06-20 23:31:14 +0700

    Update

---
+++ script.js
@@ -0,0 +1,3 @@
+// Hiện tại không cần JS đặc biệt, nhưng bạn có thể thêm tương tác sau này.
+// Ví dụ: scroll về mục sản phẩm, hiệu ứng hoặc form liên hệ.
+console.log("Website hoa lan đã được tải!");
